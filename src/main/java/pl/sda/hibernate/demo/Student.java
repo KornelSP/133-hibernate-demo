e74162f85ed9c8f0736b667a628b8635e2af3bf8
Kornel <kornelkrzyzan@gmail.com>
2022-11-26 12:44:36 +0100

    Dodanie nowej encji do bazy danych

--- src/main/java/pl/sda/hibernate/demo/Student.java
+++ src/main/java/pl/sda/hibernate/demo/Student.java
@@ -1,2 +1,26 @@
-package pl.sda.hibernate.demo;public class Student {
+package pl.sda.hibernate.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+// Klasa POJO => Plain Old Java
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Student {
+    @Id // PRIMARY KEY
+    @GeneratedValue (strategy = GenerationType.IDENTITY) //AUTO_INCERMENT
+    private Long id;
+
+    private String imie;
+    private LocalDate dataUrodzenia;
+    private String kierunekNauczania;
+    private String indeks; //  "index" jest słowem zabronionym przez mysql
 }
