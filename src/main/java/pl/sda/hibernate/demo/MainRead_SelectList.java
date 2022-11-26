844c5549a83c21aca74ed4fc091392e3d61a770c
Kornel <kornelkrzyzan@gmail.com>
2022-11-26 14:05:14 +0100

    Update rekordu

--- src/main/java/pl/sda/hibernate/demo/MainRead_SelectList.java
+++ src/main/java/pl/sda/hibernate/demo/MainRead_SelectList.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 
 import java.util.List;
 
-public class MainSelectByld {
+public class MainRead_SelectList {
     public static void main(String[] args) {
         // wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
         try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
--- src/main/java/pl/sda/hibernate/demo/MainUpdate_Update.java
+++ src/main/java/pl/sda/hibernate/demo/MainUpdate_Update.java
@@ -1,32 +1,32 @@
 package pl.sda.hibernate.demo;
 
-
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.time.LocalDate;
 
-public class MainInsert {
+public class MainUpdate_Update {
     public static void main(String[] args) {
         // wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
         try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+
             Transaction transaction = session.beginTransaction();
 
             Student student = Student.builder()
-                    .dataUrodzenia(LocalDate.of(1990,1,3))
-                    .kierunekNauczania("Informatyka")
-                    .indeks("123123Main")
-                    .imie("Paweł")
+                    .dataUrodzenia(LocalDate.of(1995,6,15))
+                    .kierunekNauczania("Filozofia")
+                    .indeks("321321")
+                    .imie("Gaweł")
+                    .id(2L)
                     .build();
 
-
-            session.persist(student);
+            session.merge(student);
 
             transaction.commit();
 
         } catch (Exception ioe) {
             // jeśli złapiemy błąd, to wywoła się catch
-
+            System.err.println("Błąd bazy: " + ioe);
         }
     }
 }
